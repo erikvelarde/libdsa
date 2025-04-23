2504e09942ca83188f523b58c1c666eafd07a7d2
freddyvelarde <freddvel.dev@gmail.com>
2025-04-22 22:33:38 -0400

    removing some unnecesary init code

--- include/libdsa/my_log.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_LOG_H
-#define MY_LOG_H
-
-void my_log(const char *message);
-
-#endif
--- src/my_log.c
+++
@@ -1,6 +0,0 @@
-#include "libdsa/my_log.h"
-#include <stdio.h>
-
-void my_log(const char *message) {
-  printf("%s\n", message);
-}
